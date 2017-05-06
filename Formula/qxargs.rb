976f12605f8f4b3417ffd67d7d4cad3ecdf665c0
iwat <iwatth@gmail.com>
2017-05-06 16:05:52 +0700

    Add bottle location

--- Formula/qxargs.rb
+++ Formula/qxargs.rb
@@ -8,6 +8,13 @@ class Qxargs < Formula
   url "https://github.com/iwat/qxargs/archive/1.0.0.tar.gz"
   sha256 "47029c5c4bfd823f7c303adae56db0ad8e1dcbec66ad2ba25959b59c90d1cdc7"
 
+  bottle do
+    root_url "https://github.com/iwat/qxargs/releases/download/1.0.0"
+
+    cellar :any_skip_relocation
+    sha256 "40088a1122ab7c86b71d2e18e220ff78aa657e30a3b59c7aa112be04130c1be5" => :sierra
+  end
+
   depends_on "go" => :build
 
   def install
